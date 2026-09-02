969461f65d35b466688891fbcdc2c16b21bbf065
nnurjanahsyam-collab <nnurjanahsyam@gmail.com>
2026-09-02 23:17:46 +0800

    Create Days1.java

---
+++ Days1.java
@@ -0,0 +1,16 @@
+public class Nurjanah { // ini adalah struktur utama untuk membungkus program yang akan dijalankan, untuk "main" ini di rubah sesuai dengan nama file javanya jvm bisa menjalankan programnya
+
+        public static void main(String[] args) {   //ini adalah untuk menjalankan program utama
+          //double a = 10.0; selain mengomentari baris bisa juga untuk membuat sintaks tidak dijalaknkan
+            doubel b = 10.0.; // tercetak karena tanda "//" ada dibelakang barisnya
+
+
+            /*
+            nah selain garis "//"
+            kita juga bisa menggunakann tanda /* + tab
+            untuk mengomentari beberapa baris sekaligus
+            */
+
+    } 
+
+}
