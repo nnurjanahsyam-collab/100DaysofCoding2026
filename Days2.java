49cdbedb4d4e2ee023e1359b3957ce8b3fa4a033
nanajungler <nnurjanahsyam@gmail.com>
2026-09-03 10:31:15 +0800

    Create Days2.java

---
+++ Days2.java
@@ -0,0 +1,14 @@
+public class daytwo {
+    public static void main(String[] args) {
+        String nama = "Nurjanah";
+        int umur = 18;
+        double tinggi = 160.0;
+
+        System.out.printf("nama : %s\n", nama);
+
+        System.out.print("umur : " + umur);
+
+        System.out.println("tinggi : " + tinggi);
+
+    }
+}
