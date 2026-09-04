3381b649a7d648012fc69ee2b6b2fabd2ba28886
nanajungler <nnurjanahsyam@gmail.com>
2026-09-04 15:42:11 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,15 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("\"Diantara pergi dan merelakan\""); // untuk menambah tanda kutip dua di ouput
+        System.out.println("Yang pergi, ajarkan ikhlas\n"); // digunakan untuk menambah baris baru
+        System.out.println("Diamm\b adalah cara bertahan "); // ini untuk menghapus satu karakter yang salah
+        System.out.println("\'Luka mengubah cara pandang\'");// untuk menambah tanda kutip satu
+        System.out.print("matilah rumah bagi diri sendiri"); //mengganti atau menimpa nilai yang salah
+        System.out.println("\rJad");
+        System.out.println("Tak semua langkah perlu ditemani\f"); //membuat paragraf baru
+        System.out.println("\\Kenangan cukup diletakkan jauh\\"); //menambah slash di ouput
+        System.out.println("Kita pernah menjadi cerita");
+        System.out.println("by\tAKBAR");                          //membuat spasi panjang di ouput
+    }
+
+}
