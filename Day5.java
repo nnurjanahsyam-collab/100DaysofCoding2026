a4aff21a86f80b4a328eaf8d56a9359fdbfdafe8
nanajungler <nnurjanahsyam@gmail.com>
2026-09-05 22:20:41 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class day5 {
+    public static void main(String[] args) {
+        System.out.println("\"tipe data pecahan (double,float)\"\f");
+        double TinggiBadan = 160.5;
+        float NilaiUjian = 80.5f;
+        // untuk kedua tipe data ini sama-sama memakai angka desimal, perbedaannya di
+        // float harus ada huruf 'f' jika tidak ada maka akan eror
+        // nah kalau double tidak usah pake huruf cukup angka saja.....
+
+        System.out.println("Tinggi badan Nurjanah : " + TinggiBadan + "\t\"double\"");
+        System.out.println("Nilai ujian yang dia dapatkan : " + NilaiUjian + "\t\"float\"");
+
+    }
+
+}
