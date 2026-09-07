987cf794320ed8f085f6b5024624d63c976dac0d
nanajungler <nnurjanahsyam@gmail.com>
2026-09-07 11:42:17 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class day6 {
+    public static void main(String[] args) {
+        boolean statusMahasiswa = true; // boolean itu hanya dapat menyimpan dua nilai yaitu true/false
+        char jenisKelamin = 'P'; // char hanya dapat menyimpan satu karakter dan memakai satu tanda kutip
+
+        System.out.println("status mahaiswa : " + statusMahasiswa);
+        System.out.println("jenis kelamin : " + jenisKelamin);
+    }
+}
