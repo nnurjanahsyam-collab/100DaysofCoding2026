66ff17d6506cdaf574149281cfdaaa3705478c24
nanajungler <nnurjanahsyam@gmail.com>
2026-09-08 11:38:11 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,14 @@
+public class day7 {
+    public static void main(String[] args) {
+        String nama = "nurjanah";
+        String angka = "20";
+        String angka1 = "30";
+        // untuk tipe data string ini menampung teks atau kalimat dan juga dia bersifat
+        // nonprimitif, tipe data ini menggunakan tanda kutip dua
+
+        System.out.println("nama saya : " + nama);
+        System.out.println(angka + angka1);
+
+    }
+
+}
