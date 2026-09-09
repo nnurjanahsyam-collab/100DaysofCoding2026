81d20740e8791da088da41f1fd73c5662914ebae
nanajungler <nnurjanahsyam@gmail.com>
2026-09-09 15:03:54 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+public class day8 {
+    public static void main(String[] args) {
+        // Deklarasi variable
+        String nama;
+
+        // inisialisasi
+        double tinggi = 160.5;
+
+        // update variable
+        int umur = 17;
+        umur = 18;
+
+        System.out.println("Tinggi saya : " + tinggi);
+        System.out.println("umur saya : " + umur);
+    }
+
+}
