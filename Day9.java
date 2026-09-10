d828b48e4770fd71f97ed9eb644bc68a87d0004c
nanajungler <nnurjanahsyam@gmail.com>
2026-09-10 16:53:55 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,9 @@
+public class day9 {
+    public static void main(String[] args) {
+        final int MAX_NILAI = 100; // nilai konstanta tidak boleh diubah setelah ditetapkan
+
+        System.out.println("MAX_NILAI : " + MAX_NILAI);
+
+    }
+
+}
