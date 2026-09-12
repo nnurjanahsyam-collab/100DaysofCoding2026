82f532b687b145153494875f91079b10164e0356
nanajungler <nnurjanahsyam@gmail.com>
2026-09-12 08:25:56 +0800

    Create Day11.java

---
+++ Day11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Nama;
+        int Umur;
+        double Tinggi;
+
+        System.out.print("Nama : ");
+        Nama = sc.nextLine();
+        System.out.print("Umur : ");
+        Umur = sc.nextInt();
+        System.out.print("Tinggi : ");
+        Tinggi = sc.nextDouble();
+
+    }
+
+}
