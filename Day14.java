df15059b3675604efdba0d8884f08bc24eab9310
nanajungler <nnurjanahsyam@gmail.com>
2026-09-15 21:01:14 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+public class day14{    public static void main(String[] args) {
+        //operasi aritmatika penjumlahn dan pengurangan 
+        int a = 10;
+        int b = 5;
+
+        int total = a + b;
+        System.out.println("Total penjumlahan dari " + a +"+" + b + " = " + total);
+        int hasil = a - b;
+        System.out.println("Total pengurangan dari " + a + "-" + b + " = " + hasil);
+
+
+
+        
+    }
+                  }
