9f044f54cde2f8bad0d8f463d0e9aaf51b13420e
nanajungler <nnurjanahsyam@gmail.com>
2026-09-16 19:09:48 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class day15 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        int hasil = a * b;
+        System.out.println("Total dari perkalian " + " a " + "*" + " b " + " = " + hasil );
+        int total = a / b;
+        System.out.println("Total dari pembagian " + " a " + "/" + " b" + " = " + total );
+
+        
+        
+    }
+
+    
+}
