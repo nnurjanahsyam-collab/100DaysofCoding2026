b6690819d03bcc85db32c01c0e7348d79492231d
nanajungler <nnurjanahsyam@gmail.com>
2026-09-17 19:30:15 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,12 @@
+public class day16 {
+    public static void main(String[] args) {
+        // operator aritmatika modulus atau sisa bagi
+        int a = 5;
+        int b = 3;
+
+        int hasil = a % b;
+
+        System.out.println(hasil);
+
+    }
+}
