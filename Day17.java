2d789819dcbee9c9f7ecfc056514f5fe05c6b9b4
nanajungler <nnurjanahsyam@gmail.com>
2026-09-18 20:07:03 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,25 @@
+public class day17 {
+    public static void main(String[] args) {
+        // penugasan assignment
+        int a = 10;
+        int b = 5;
+        int c = 2;
+        int d = 6;
+        int e = 8;
+
+        // operator penugasan dalam aritmatika
+        a += 5;
+        b -= 3;
+        c *= 2;
+        d /= 2;
+        e %= 3;
+
+        System.out.println("nilai a + 5 : " + a);
+        System.out.println("nilai b - 3 : " + b);
+        System.out.println("nilai c * 2 : " + c);
+        System.out.println("nilai d / 2 : " + d);
+        System.out.println("nilai e % 3 : " + e);
+
+    }
+
+}
