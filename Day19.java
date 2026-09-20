275990729dacf191a89ba9cbee31f16820caadb5
nanajungler <nnurjanahsyam@gmail.com>
2026-09-20 18:51:33 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class day19 {
+    public static void main(String[] args) {
+        //koversi manual/paksa tipe data besar ke kecil
+        int a = 350;
+        byte b = (byte) + a;
+
+        System.out.println(b);
+
+    }
+
+}
