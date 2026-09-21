9803d73de4049a15fbd226911486880234c2e998
nanajungler <nnurjanahsyam@gmail.com>
2026-09-21 21:34:05 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,8 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int a = 10;
+        String b = String.valueOf(a);
+        System.out.println("Hasil setelah konversi ke String  : " + b + b);
+
+    }
+}
