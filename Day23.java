cce7620d0be02a04869784f949e9ffb60e5554b8
nanajungler <nnurjanahsyam@gmail.com>
2026-09-24 13:03:54 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,11 @@
+public class Day23 {
+    public static void main(String[] args) {
+        //membuat program luas persegi
+        int sisi1 = 15;
+        int sisi2 = 2;
+
+        int luas = sisi1 * sisi2;
+        System.out.println("luas: " + luas);
+    }
+    
+}
