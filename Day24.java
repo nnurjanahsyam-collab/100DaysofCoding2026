4af574f62a08fbe57beba36425036c8139941639
nanajungler <nnurjanahsyam@gmail.com>
2026-09-25 20:50:21 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,11 @@
+public class Day24{
+    public static void main(String[] args) {
+        //program menghitung luas persegi panjang
+        int panjang = 10;
+        int lebar = 5;
+
+        int luas = panjang * lebar;
+        System.out.println("Luas: " + luas);
+        
+    }
+}
