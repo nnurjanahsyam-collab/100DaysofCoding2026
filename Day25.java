731e7bd8248dbebbe696bd6d2bd1a5c66dd86415
nanajungler <nnurjanahsyam@gmail.com>
2026-09-26 20:44:12 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,16 @@
+public class Day25 {
+    public static void main(String[] args) {
+        double j = 3.14;
+        double r = 5;
+        double luas, kll;
+
+        luas = j * r * r;
+        kll = 2 * j * r;
+        System.out.println("Luas: " + luas);
+        System.out.println("keliling: " + kll);
+
+
+
+    }
+    
+}
